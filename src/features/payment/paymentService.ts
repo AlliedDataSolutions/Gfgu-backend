0f71309a49ec38cf953f1a7988895cd876422aa3
Adegbite femi <40705708+Efhemo@users.noreply.github.com>
2025-03-26 19:33:08 -0600

    payment service

--- src/features/payment/paymentService.ts
+++ src/features/payment/paymentService.ts
@@ -1,5 +1,6 @@
 import paypal from "@paypal/checkout-server-sdk";
 import { client } from "@src/config/paypal";
+import paypalPayouts from "@paypal/payouts-sdk";
 
 export class PaymentService {
   async createOrder(amount: string) {
@@ -23,5 +24,30 @@ export class PaymentService {
     });
     const response = await client.execute(request);
     return { id: response.result.id };
+  }
+
+  // admin to send money to vendors.
+  async payout(vendorEmail: string, amount: string) {
+    const currency = "CAD";
+
+    const request = new paypalPayouts.payouts.PayoutsPostRequest();
+    request.requestBody({
+      sender_batch_header: {
+        sender_batch_id: `batch-${Date.now()}`,
+        email_subject: "You have received a payment!",
+      },
+      items: [
+        {
+          recipient_type: "EMAIL",
+          amount: {
+            currency,
+            value: amount,
+          },
+          receiver: vendorEmail,
+        },
+      ],
+    });
+    const response = await client.execute(request);
+    return response.result;
   }
 }
