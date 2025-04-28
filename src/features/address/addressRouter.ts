65600949d6893df4ece2e2dd0c20cf02a5a1753d
Yussuf <106894981+tayojs@users.noreply.github.com>
2025-04-28 08:29:17 -0600

    Include endpoints to add and remove address

--- src/features/address/addressRouter.ts
+++ src/features/address/addressRouter.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { authMiddleware } from "../../middlewares/authMiddleware";
-import { getUserAddresses, addUserAddress } from "./addressController";
+import { getUserAddresses, addUserAddress, deleteUserAddress, updateUserAddress } from "./addressController";
 
 const router = Router();
 
+router.use(authMiddleware);
+
 router.get("/", getUserAddresses);
 router.post("/", addUserAddress);
+
+router.delete("/:id", deleteUserAddress);   
+router.put("/:id", updateUserAddress);
 
 export default router;
